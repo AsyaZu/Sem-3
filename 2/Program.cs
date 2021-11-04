6dbf6dae0e1e4d98ed7fb7d3f031b4b5060138c7
Asya <nastya.Z1998@yandex.ru>
2021-11-04 21:07:20 +0500

    2

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿ //2. Даны два числа. Показать большее и меньшее число
+
+void a(int a, int b)
+{
+    if (a > b)
+    {
+        Console.WriteLine($"большее число = {a}");
+        Console.WriteLine($"меньшее число = {b}");
+    }
+    else if (a < b)
+    {
+        Console.WriteLine($"большее число = {b}");
+        Console.WriteLine($"меньшее число = {a}");
+    }
+    else
+    {
+        Console.WriteLine("числа равны");
+    }
+
+}
+
+a(7, 7);
