8ab66fe84e0d55bb130e4531f6854d0ba3abc1c3
Asya <nastya.Z1998@yandex.ru>
2021-11-04 22:19:23 +0500

    7

---
+++ 7/Program.cs
@@ -0,0 +1,13 @@
+﻿//7. Показать числа от -N до N
+
+void ShowNumbers (int a, int b)
+{   
+    int NextNumber = a;
+    while (NextNumber <= b)
+    {
+        Console.WriteLine(NextNumber);
+        NextNumber++;
+    }
+}
+ShowNumbers(-3, 3);
+
