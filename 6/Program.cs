8d2d00eeb910f57952bc9d447048f4085f08e9a5
Asya <nastya.Z1998@yandex.ru>
2021-11-04 21:42:19 +0500

    6

---
+++ 6/Program.cs
@@ -0,0 +1,16 @@
+﻿//6. Выяснить является ли число чётным
+
+void FindEven (int number)
+{
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("число является четным");
+    }
+    else
+    {
+        Console.WriteLine("число является нечетным");
+    }
+
+}
+
+FindEven(35);
