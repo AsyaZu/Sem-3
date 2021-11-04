2ceb55eb06416823d1afe85f3b2402a3a9b3d84d
Asya <nastya.Z1998@yandex.ru>
2021-11-04 21:16:18 +0500

    4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿//4. Найти максимальное из трех чисел
+
+int FindMax (int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+     if (c > max)
+    {
+        max = c;
+    }
+
+return max;
+}
+
+int res = FindMax (12,35,66);
+Console.WriteLine(res);
