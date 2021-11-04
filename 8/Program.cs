fea1be7e799368fa2d05df24a0c87d4addf25e1c
Asya <nastya.Z1998@yandex.ru>
2021-11-04 22:35:28 +0500

    8

---
+++ 8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать четные числа от 1 до N
+
+void ShowNumbers(int a, int b)
+{
+    int NextNumber = a;
+    while (NextNumber <= b)
+    {
+        if (NextNumber % 2 == 0)
+        {
+            Console.WriteLine(NextNumber);
+        }
+        NextNumber++;
+    }
+}
+ShowNumbers(1, 30);
