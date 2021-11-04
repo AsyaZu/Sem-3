730c39aace9082e620558dd2bb8ecb9b8bc8b88e
Asya <nastya.Z1998@yandex.ru>
2021-11-04 21:36:20 +0500

    5

---
+++ 5/Program.cs
@@ -0,0 +1,9 @@
+﻿//5. Написать программу вычисления значения функции y=f(a)/  sin
+
+double FindSin (double leg, double hypo)
+{
+    double sin = leg / hypo;
+    return sin;
+}
+
+ Console.WriteLine(FindSin(51, 100));
