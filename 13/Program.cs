c6c348160e1052b11cc641750c39ac5e0fd697eb
Asya <nastya.Z1998@yandex.ru>
2021-11-05 17:35:19 +0500

    13

---
+++ 13/Program.cs
@@ -0,0 +1,14 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+void multiplicity (int dividend, int divider)
+{
+    if (dividend % divider == 0)
+    {
+        Console.WriteLine("Кратно");
+    } 
+    else 
+    {
+        Console.WriteLine(dividend % divider);
+    }
+}
+multiplicity(7, 2);
