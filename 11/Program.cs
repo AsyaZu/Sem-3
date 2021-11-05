cfdfb8669f2507451c9c1498e95e10afe05bf935
Asya <nastya.Z1998@yandex.ru>
2021-11-05 17:12:22 +0500

    11

---
+++ 11/Program.cs
@@ -0,0 +1,20 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Number (int n)
+{
+    int second = (n % 10);
+    int first = (n - second) / 10;
+    if (first > second)
+    {
+        return first;
+    }
+    else
+    {
+        return second;
+    }
+    
+}
+
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+Console.WriteLine(Number(n));
