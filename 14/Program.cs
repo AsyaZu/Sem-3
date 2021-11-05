031a152f3507f5562a495cfcfbb05ac4a131c615
Asya <nastya.Z1998@yandex.ru>
2021-11-05 18:26:45 +0500

    14

---
+++ 14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти третью цифру числа или сообщить, что её нет (с конца)
+
+void number(int a)
+{
+    int res = ((a % 1000) - ((a % 1000) % 100)) / 100;
+    if (res == 0)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine(res);
+    }
+}
+
+number(55933);
