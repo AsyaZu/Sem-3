468c2526d6bd283ab9af073aefb0358b962b20a1
Asya <nastya.Z1998@yandex.ru>
2021-11-05 17:20:45 +0500

    12

---
+++ 12/Program.cs
@@ -0,0 +1,10 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+int Number (int n)
+{
+    int first = (n - (n % 100)) / 10;
+    int second = (n % 100) % 10;
+    int res =  first + second;
+    return res;
+}
+Console.WriteLine(Number(723));
