221e4c6a94c742c5840f4d497f81218aa02c45ca
Asya <nastya.Z1998@yandex.ru>
2021-11-06 18:15:29 +0500

    changed 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,14 +1,14 @@
 ﻿//1. По двум заданным числам проверять является ли первое квадратом второго
 
-void result(int a, int b)
+string result (int a, int b)
 {
     if (b == a * a)
     {
-        Console.WriteLine("Число является квадратом");
+        return ("Число является квадратом");
     }
     else
     {
-        Console.WriteLine("Число не является квадратом");
+        return ("Число не является квадратом");
     }
 }
- result(5, 25);
+ Console.WriteLine(result(5, 26));
