7845705b095e9199630d7105ec55efa0b4945f79
Asya <nastya.Z1998@yandex.ru>
2021-11-08 17:54:52 +0500

    changed 18

---
+++ 18/Program.cs
@@ -0,0 +1,14 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool F(bool X, bool Y)
+{
+    bool res = false;
+    bool first = !(X||Y);
+    bool second = !X && !Y;
+    if (first == second)
+    {
+        res = true;
+    }
+    return res;
+}
+Console.WriteLine(F(false, true));
+
