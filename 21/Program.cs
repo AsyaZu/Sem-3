77da13da7d08ff561449ea38ae6049490b6bd501
Asya <nastya.Z1998@yandex.ru>
2021-11-09 16:56:40 +0500

    changed 21

---
+++ 21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+bool Palindrome (int n)
+{
+    bool res = false;
+    int fifth = n % 10;
+    int first = (n - (n % 10000)) / 10000;
+    int second = (n % 10000 - n % 1000) / 1000;
+    int fourth = (n % 100 - first) / 10;
+    if (first == fifth && second == fourth)
+    {
+        res = true;
+    }
+    return res;
+}
+Console.WriteLine(Palindrome(71431));
