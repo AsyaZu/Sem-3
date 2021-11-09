973154722d920150dd9e994601591a388a0f004d
Asya <nastya.Z1998@yandex.ru>
2021-11-09 19:36:03 +0500

    changed 22.2

---
+++ 22.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти расстояние между точками в пространстве 3D
+
+double FindH (int xa, int ya, int xb, int yb, int zx, int zy)
+{
+    int X = xb -xa;
+    int Y = yb - ya;
+    int Z = zx - zy;
+    double result = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) +  Math.Pow(Z, 2));
+    return result;
+}
+Console.WriteLine(FindH(4, 5, 9, 2, 7, 12));
