bc0c5f7508a58aecd2807f970b0b5ca43f1c205b
Asya <nastya.Z1998@yandex.ru>
2021-11-09 19:54:10 +0500

    changed 23

---
+++ 23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+void SquaresTable (int N)
+{
+    int res = 0;
+    for (int n = 1; n <= N; n++)
+    {
+        res = n * n;
+        Console.WriteLine($"{n}^2 = {res}");
+    }
+}
+SquaresTable(15);
