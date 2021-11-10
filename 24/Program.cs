b50b5cbe5963c1f6b1078bdb87f4dba0b98152c8
Asya <nastya.Z1998@yandex.ru>
2021-11-10 07:57:40 +0500

    changed 24

---
+++ 24/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+
+void SquaresTable (int N)
+{
+    int res = 0;
+    for (int n = 1; n <= N; n++)
+    {
+        res = n * n * n;
+        Console.WriteLine($"{n}^3 = {res}");
+    }
+}
+SquaresTable(15);
