bf148dbc6fd6d399fd3ae105e87c5e96b8f8225d
Asya <nastya.Z1998@yandex.ru>
2021-11-10 08:41:42 +0500

    changed 26

---
+++ 26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int Degree (int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        res = res * a;
+        b = b - 1;
+    }
+    return res;
+}
+Console.WriteLine(Degree(4, 3));
