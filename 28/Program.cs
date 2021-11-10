b36bb33845438eff7ccb0c354c51938d6ec06b49
Asya <nastya.Z1998@yandex.ru>
2021-11-10 17:01:15 +0500

    changed 28

---
+++ 28/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+
+int Sum (int number)
+{
+    int sum = 0;
+    while (number >= 1)
+    {
+        sum = sum + number % 10;
+        number = (number - number % 10) / 10;
+    }
+    return sum;
+}
+Console.WriteLine(Sum(0));
