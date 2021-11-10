ceacd6f712750c7232614574e9143ed40fd8ef97
Asya <nastya.Z1998@yandex.ru>
2021-11-10 16:48:28 +0500

    changed 27

---
+++ 27/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить количество цифр в числе
+
+int Count (int number)
+{
+    int count = 0;
+    while (number >= 1)
+    {
+        number = (number - number % 10) / 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(Count(156));
