be984cad7e26ff08eaff6b074b7c00349e4be861
Asya <nastya.Z1998@yandex.ru>
2021-11-10 17:38:08 +0500

    changed 30

---
+++ 30/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+void Cube (int N)
+{
+    for (int n = 1; n <= N; n++)
+    {
+        int res = n * n * n;
+        if (res % 10 % 2 == 0)
+        {
+            Console.WriteLine($"{n}^3 = {res}");
+        }
+    }
+}
+Cube(16);
