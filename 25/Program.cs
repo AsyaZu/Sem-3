8a740ee663b9a036dce89bd736dc1e9c0b0a9475
Asya <nastya.Z1998@yandex.ru>
2021-11-10 08:10:32 +0500

    changed 25

---
+++ 25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму чисел от 1 до А
+int Sum (int A)
+{
+    int sum = 0;
+    for (int n = 1; n <= A; n++)
+    {
+        sum = n + sum;
+    }
+    return sum;
+}
+Console.WriteLine(Sum(50));
