2b262cefccfa86bfe6e7f80acf39239a5be630b9
Asya <nastya.Z1998@yandex.ru>
2021-11-10 17:17:25 +0500

    changed 29

---
+++ 29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int Multiplication (int N)
+{
+    int mult = 1;
+    for (int n = 1; n <= N; n++)
+    {
+        mult = mult * n;
+    }
+    return mult;
+}
+Console.WriteLine(Multiplication(5));
