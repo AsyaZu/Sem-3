8c67284b20e7fae4253698c28d8bc66ba22da612
Asya <nastya.Z1998@yandex.ru>
2021-11-11 15:55:57 +0500

    changed 22.1_1

--- 22.1/Program.cs
+++ 22.1/Program.cs
@@ -1,10 +1,21 @@
 ﻿// Найти расстояние между точками в пространстве 2D
 
-double FindH (int xa, int ya, int xb, int yb)
+//double FindH (int xa, int ya, int xb, int yb)
+//{
+//    int legX = xb -xa;
+//    int legY = yb - ya;
+//    double result = Math.Sqrt(Math.Pow(legX, 2) + Math.Pow(legY, 2));
+//    return result;
+//}
+//Console.WriteLine(FindH(4, 5, 9, 2));
+
+double FindH2 ((int x, int y) a, (int x, int y) b)
 {
-    int legX = xb -xa;
-    int legY = yb - ya;
+    int legX = b.x -a.x;
+    int legY = b.y - a.y;
     double result = Math.Sqrt(Math.Pow(legX, 2) + Math.Pow(legY, 2));
     return result;
 }
-Console.WriteLine(FindH(4, 5, 9, 2));
+
+
+Console.WriteLine(FindH2((4, 5), (9, 2)));
