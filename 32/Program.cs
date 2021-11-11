4a72c53e02c09b2a27a5e7c55477fb829e3d6d87
Asya <nastya.Z1998@yandex.ru>
2021-11-11 17:35:44 +0500

    32

--- 32/Program.cs
+++ 32/Program.cs
@@ -1,21 +1,25 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
 
-int[] array = new int [8];
+int[] array = new int[8];
 
- string PrintArray (int[] array)
- {
-    string res = string.Empty;
+void FillArray(int[] array)
+{
     int length = array.Length;
-     for (int i = 0; i < length; i++)
-     {
-         array[i] = new Random().Next(0, 2); //[0; 2)
-     }
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
 
-     for (int i = 0; i < length; i++)
-     {
+string PrintArray(int[] array)
+{
+    string res = string.Empty;
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
         res += $"{array[i]}  ";
-     }
+    }
     return res;
- }
-
+}
+FillArray(array);
 Console.WriteLine(PrintArray(array));
