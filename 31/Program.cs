0af7d2f7ed450449c0334a173db40772124c0fd7
Asya <nastya.Z1998@yandex.ru>
2021-11-11 13:40:30 +0500

    changed 31

---
+++ 31/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] array = new int [8];
+
+ string PrintArray (int[] array)
+ {
+    string res = string.Empty;
+    int length = array.Length;
+     for (int i = 0; i < length; i++)
+     {
+         array[i] = new Random().Next(0, 10);
+     }
+
+     for (int i = 0; i < length; i++)
+     {
+        res += $"{array[i]}  ";
+     }
+    return res;
+ }
+
+Console.WriteLine(PrintArray(array));
