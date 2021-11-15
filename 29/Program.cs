c63901210f9224b3e7083073ee9115fed590d501
Asya <nastya.Z1998@yandex.ru>
2021-11-15 14:20:15 +0500

    29

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,12 +1,21 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
 
+//double Multiplication (int A)
+//{
+//    double mult = 0;
+//    for (int n = 1; n <= A; n++)
+//    {
+//        sum = n + sum;
+//    }
+//    return sum;
+//}
+
 int Multiplication (int N)
 {
-    int mult = 1;
-    for (int n = 1; n <= N; n++)
-    {
-        mult = mult * n;
-    }
-    return mult;
+    if (N == 1) return 1;
+    return N * Multiplication(N - 1); 
 }
-Console.WriteLine(Multiplication(5));
+
+DateTime dt = DateTime.Now;
+Console.WriteLine(Multiplication(1));
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
