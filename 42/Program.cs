4d1a5752410bec34580888f6060ef52e41e3e3b3
Asya <nastya.Z1998@yandex.ru>
2021-11-15 14:08:00 +0500

    42

---
+++ 42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.WriteLine("Введите числа");
+int[] array = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+int Numbers (int[] array)
+{
+    int length = array.Length;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(Numbers(array));
+
