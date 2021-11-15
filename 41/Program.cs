746b3d32bb2e9c91921a9a2ec8e6c8a6cb8d342a
Asya <nastya.Z1998@yandex.ru>
2021-11-15 13:34:23 +0500

    41

---
+++ 41/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить являются ли три числа сторонами треугольника 
+bool Find (int a, int b, int c)
+{
+    if (a > b && a > c && a < (b + c)) return true;
+    if (b > a && b > c && b < (a + c)) return true;
+    if (c > a && c > b && c < (a + b)) return true;
+    else return false;
+}
+
+Console.WriteLine(Find(3, 4, 7));
