76b703429c0e90fcd5f72612954171bcb884f6f2
Asya <nastya.Z1998@yandex.ru>
2021-11-16 16:18:15 +0500

    39

---
+++ 38/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = {12, 101, 8, 16, 15, 17, 41, 2};
+
+int Sum (int[] array)
+{
+    int sum = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine(Sum(array));
