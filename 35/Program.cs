aaab46f0ed32e7488b6b19d2aca8130867536f35
Asya <nastya.Z1998@yandex.ru>
2021-11-16 14:19:48 +0500

    35

---
+++ 35/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число 
+
+int[] array = {12, 5, -9, 11};
+bool Find (int[] array, int N)
+{
+    bool res = false;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] == N) 
+        {
+            res = true;
+        }
+    }
+    return res;
+}
+Console.WriteLine(Find(array, 12));
+
