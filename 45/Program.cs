b7d6b4e03e7a82364bd81ee44b3a5c8c69275705
Asya <nastya.Z1998@yandex.ru>
2021-11-20 12:30:06 +0500

    45

---
+++ 45/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа Фибоначчи
+
+double[] array = new double [50];
+
+string Fib (double[] array)
+{
+    int length = array.Length;
+    int i = 0;
+    array[0] = 0;
+    array[1] = 1;
+    string res = $"{array[0]} {array[1]} ";
+    for (i = 2; i < length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+        res += $"{array[i]} ";
+    }
+    return res;
+}
+Console.WriteLine(Fib(array));
