4383384abffdd6e89bfcba6be122e646fe9391d7
Asya <nastya.Z1998@yandex.ru>
2021-11-24 14:04:47 +0500

    69

---
+++ 69/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string Numbers (int M, int N)
+{
+    string res = string.Empty;
+    for (int n = M; n <= N; n++)
+    {
+        res = $"{Numbers(M, n - 1) + n} ";
+    }
+    return res;
+}
+Console.WriteLine(Numbers(-10, 10));
