8abfbb0823c91f971c574e5a0e625ce8cd147249
Asya <nastya.Z1998@yandex.ru>
2021-11-24 12:12:52 +0500

    52

---
+++ 52/Program.cs
@@ -0,0 +1,47 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+int[,] matrix = new int[6, 6];
+
+void FillArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i,j] = matrix[i,j]*matrix[i,j];
+            } 
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
