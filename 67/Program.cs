45f3602671e67fdeac65d8af867e40224639cd92
Asya <nastya.Z1998@yandex.ru>
2021-11-24 13:52:38 +0500

    67

---
+++ 67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+string Numbers (int N)
+{
+    string res = string.Empty;
+    for (int n = 1; n <= N; n++)
+    {
+        res = $"{Numbers(n - 1) + n} ";
+    }
+    return res;
+}
+Console.WriteLine(Numbers(10));
