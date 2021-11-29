86fe28229fea424593936f2ce78cefa8b0d02586
Asya <nastya.Z1998@yandex.ru>
2021-11-29 13:17:34 +0500

    51

---
+++ 51/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int m = 3;
+int n = 4;
+int[,] matrix = new int[m, n];
+
+void PrintArray(int[,] matrix)
+{
+    for (int m = 0; m < matrix.GetLength(0); m++)
+    {
+        for (int n = 0; n < matrix.GetLength(1); n++)
+        {
+            matrix[m, n] = m + n; 
+            Console.Write(matrix[m, n]);
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
