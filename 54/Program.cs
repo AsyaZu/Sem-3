9128d6d11bf37734a338a2649f677a2a15ea8875
Asya <nastya.Z1998@yandex.ru>
2021-12-01 10:52:56 +0500

    54

---
+++ 54/Program.cs
@@ -0,0 +1,47 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+int[,] matrix = new int[4, 4];
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Sum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine(Sum(matrix));
