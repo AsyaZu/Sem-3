2642073acb7b176d476fca6ecf7dd03c5aae66f4
Asya <nastya.Z1998@yandex.ru>
2021-12-02 14:13:33 +0500

    changed 70

--- 70/Program.cs
+++ 70/Program.cs
@@ -1,11 +1,9 @@
-﻿// Найти сумму элементов от M до N, N и M заданы
+﻿// Найти сумму элементов от M до N, N и M задан
+
 int Sum(int M, int N)
 {
-    int sum = 0;
-    for (int i = M; i <= N; i++)
-    {
-        sum = i + Sum(M, i - 1);
-    }
-    return sum;
+    if (M == N) return N;
+    return M + Sum(M+1, N);
 }
-Console.WriteLine(Sum(-10, 5));
+Console.WriteLine(Sum(2, 4));
+
