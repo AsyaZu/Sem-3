8cf5edc8df1d5edd600f782769a685509cdc026d
Asya <nastya.Z1998@yandex.ru>
2021-12-02 17:15:21 +0500

    56

---
+++ 56/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] matrix = new int[3, 3];
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] matrix)
+{
+    int i = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int n = matrix[i, j];
+        matrix[i, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = n;
+    }
+
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
+PrintArray(matrix);
