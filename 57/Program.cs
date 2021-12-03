10f04f68673953de35f7cfa8dad25b8415378c77
Asya <nastya.Z1998@yandex.ru>
2021-12-03 17:17:16 +0500

    57

---
+++ 57/Program.cs
@@ -0,0 +1,53 @@
+﻿// 57. Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива
+
+int[,] matrix = new int[3, 3];
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int jMax = 0;
+            int max = matrix[i, j];
+            if  (matrix[i, j + 1] > max)
+            {
+                max = matrix[i, j + 1];
+                jMax = j;
+            }
+            
+            
+        }
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
+PrintArray(matrix);
+
+
