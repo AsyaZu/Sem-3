2c5b2d50fbd19d8a854c9542eb8ffc1f4077049e
Asya <92664686+AsyaZu@users.noreply.github.com>
2021-12-04 17:12:27 +0500

    changed 57

--- 57/Program.cs
+++ 57/Program.cs
@@ -48,6 +48,57 @@ FillArray(matrix);
 PrintArray(matrix);
 Console.WriteLine();
 ChangeArray(matrix);
+PrintArray(matrix);// 57. Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива
+
+int[,] matrix = new int[3, 7];
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] matrix)
+{
+    int save = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j + 1] > matrix[i, j])
+                {
+                    save = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = save;
+                }
+            }
+        }
+    }
+}
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeArray(matrix);
 PrintArray(matrix);
 
 
