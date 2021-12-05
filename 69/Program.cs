8d10f19fd33334a4f5a63ac6d5dd04e628220f0d
Asya <nastya.Z1998@yandex.ru>
2021-12-05 17:30:57 +0500

    69

--- 69/Program.cs
+++ 69/Program.cs
@@ -1,8 +1,9 @@
-﻿// Показать натуральные числа от M до N, N и M заданы
+﻿// Найти сумму элементов от M до N, N и M задан
 
-string Numbers (int M, int N)
+int Sum(int M, int N)
 {
-    if (M == N) return $"{N} ";
-    return $"{Numbers(M, N - 1) + N} ";;
+    if (M == N) return N;
+    return M + Sum(M+1, N);
 }
-Console.WriteLine(Numbers(-10, 10));
+Console.WriteLine(Sum(2, 4));
+
